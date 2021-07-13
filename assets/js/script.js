db106eec493cdc06836029ec8d11d5d7ad0d05b1
nikolai <tevsnn2020@gmail.com>
2021-07-13 17:02:50 +0300

    Add task1

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+const numer = function(number1, number2) {
+    if (number1 > number2) {
+        return number1;
+    }
+
+    if (number1 < number2) {
+        return number2
+    }
+
+    if (number1 = number2) {
+        return ('поровну')
+    }
+
+    
+}
+
+const result = numer(12,12);
+console.log(result)
