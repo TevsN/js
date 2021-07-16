e65728c88687fcf60b04d972a696dca9d1fc7130
nikolai <tevsnn2020@gmail.com>
2021-07-16 17:06:48 +0300

    Add cat

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,57 @@
-const number1 = +prompt('number 1')
-const number2 = +prompt('number 2')
-const calc = prompt('Enter operator')
+const cat1 = {
+  name: "Arnold",
+  breed: "nigster",
+  age: 2,
+  color: "blue",
+  isMale: true,
+  eating: function () {
+    return "AM-NIMA-NIAM";
+  },
+};
 
-switch(calc) {
-    case '+': {
-        const result = number1 + number2;
-        alert(result);
-        break;
-    }
-    case '-': {
-        const result = number1 - number2;
-        alert(result);
-        break;
-    }
-    case '*': {
-        const result = number1 * number2;
-        alert(result);
-        break;
-    }
-    case '/': {
-        const result = number1 / number2;
-        alert(result);
-        break;
-    }
-    default: {
-        alert('Unknow operator');
-    }
+const cat2 = {
+  name: "Nastia",
+  breed: "siamskii",
+  age: 5,
+  color: "black",
+  isMale: true,
+  eating: function () {
+    return "AM-NIMA-NIAM";
+  },
+};
+
+const cat3 = {
+  name: "Bodia",
+  breed: "aligator",
+  age: 10,
+  color: "white",
+  isMale: true,
+  eating: function () {
+    return "AM-NIMA-NIAM";
+  },
+};
+
+const cat4 = {
+  name: "Pusik",
+  breed: "veslo",
+  age: 4,
+  color: "red",
+  isMale: true,
+  eating: function () {
+    return "AM-NIMA-NIAM";
+  },
+};
+
+function Cat(name, breed, isMale, color,age ) {
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+  this.color = color;
+  this.isMale = isMale;
+
+  this.eating = function () {
+    return "AM-NIMA-NIAM";
+  };
 }
 
+const constractedCat = new Cat("Алберт", "куни-мейн", true, "красный", 4);
