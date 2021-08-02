ac41cf812367a9a71ad59c45a065e14036d1a6a4
nikolai <tevsnn2020@gmail.com>
2021-08-02 16:57:30 +0300

    Add new task

--- assets/js/class.js
+++ assets/js/class.js
@@ -1,33 +1,100 @@
-class Worker {
-    constructor(name, lastName, numberDay, payMent,premium){
-        if(payMent < 0){
-            throw new RangeError('Error(228)')
-        }
+// class Worker {
+//     constructor(name, lastName, numberDay, payMent,premium, age){
+// //         if(payMent < 0){
+//             throw new RangeError('Error(228)')
+//         }
 
-        if(numberDay < 0){
-            throw new RangeError('Error(1337)') 
-        }
-        
-    this.name = name;
-    this.lastName = lastName;
-    this.numberDay = numberDay;
-    this.payment = payMent;
-    this.premium = premium;
+//         if(numberDay < 0){
+//             throw new RangeError('Error(1337)')
+//         }
+
+//     this.name = name;
+//     this.lastName = lastName;
+//     this.numberDay = numberDay;
+//     this.payment = payMent;
+//     this.premium = premium;
+//     this._age = age;
+//     }
+
+//     get age(){
+//         return this._age
+//     }
+
+//     set age(newAge){
+//         if(isNaN(newAge) || typeof newAge !== 'number'){
+//             throw new TypeError('Age mus be number');
+//         }
+//         if(newAge < 0){
+//             throw new RangeError('Age must be pensil')
+//         }
+//         this._age = newAge;
+//     }
+
+//     getSalary(){
+//         if(this.premium){
+//             return this.numberDay * this.payment * 1.25 ;
+//         }
+//         return this.numberDay * this.payment;
+
+//     }
+
+//     getFullName(){
+//         return `${this.name} ${this.lastName}`;
+//     }
+// }
+
+// const human1 = new Worker ('Arnold','Шварцлогер', 10, 100, true, -40);
+// const human2 = new Worker ('Arnold','Шварцлогер', 10, 25, false, 40);
+
+class Auto {
+  constructor(model, bodyWeight, maxSpeed, currentSpeed) {
+    this.model = model;
+    this.bodyWeight = bodyWeight;
+    this.maxSpeed = maxSpeed;
+    this.currentSpeed = currentSpeed;
+  }
+  set currentSpeed(speed) {
+    if (isNaN(speed) || typeof speed !== "number") {
+      throw new TypeError("Auto is not defined");
     }
+    if (speed < 0) {
+      throw new RangeError("З глузду зйiхав?");
+    }
+    this._currentSpeed = speed;
+  }
 
+  get currentSpeed() {
+    return this._currentSpeed;
+  }
 
-    getSalary(){
-        if(this.premium){
-            return this.numberDay * this.payment * 1.25 ; 
-        }
-        return this.numberDay * this.payment;
+  set bodyWeight(Weight) {
+    if (isNaN(Weight) || typeof Weight !== "number") {
+      throw new TypeError("Я в минус не уйду");
+    }
+    if (Weight < 0) {
+      throw new RangeError("Я в минус не уйду");
+    }
+    this._bodyWeight = Weight;
+  }
+
+  get bodyWeight() {
+    return this._bodyWeight;
+  }
+
+  set model (names) {
+    if (typeof names !== "string") {
+      throw new TypeError("Invalid ты");
+    }
     
+    if (names.trim() === '') {
+      throw Error("Я реально не понял ты дарушка?");
     }
+    this._model = names;
+  }
 
-    getFullName(){
-        return `${this.name} ${this.lastName}`;
-    }
+  get model() {
+    return this._model;
+  }
 }
 
-const human1 = new Worker ('Arnold','Шварцлогер', 10, 100, true);
-const human2 = new Worker ('Arnold','Шварцлогер', 10, 25, false);
+const Model1 = new Auto("Toyota Supra (A80)", 1340, 300, 90);
